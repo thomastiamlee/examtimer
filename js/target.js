21ba16d21d24b9cf829c7d7e9c7fb2082d4c40a4
Thomas <thomasjamestiamlee@gmail.com>
2014-08-27 23:53:45 +0800

    Added files.

---
+++ js/target.js
@@ -0,0 +1,16 @@
+
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null){
+    return null;
+  }
+  else{
+    return results[1] || 0;
+  }
+}
+
+function initialize() {
+  target = new Date($.urlParam('y'), $.urlParam('m'), $.urlParam('d'), $.urlParam('hr'), $.urlParam('min'), 0, 0);
+}
+
+initialize();
